517217993c681ad461c02a46c1bc353d872aa728
JefteMartins <jefte.martins@hotmail.com>
2022-04-02 13:43:56 -0300

    falta so ajeitar a arota do botao

--- práticasRápidas/Node-Express/Newsletter-SignUp/app.js
+++ práticasRápidas/Node-Express/Newsletter-SignUp/app.js
@@ -2,12 +2,13 @@ const mailchimp = require("@mailchimp/mailchimp_marketing");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { response } = require("express");
+const res = require("express/lib/response");
 const app = express();
 require("dotenv").config();
 
-const apiKey = process.env.API_KEY;
-const audId = process.env.AUD_ID;
-const apiKeyServer = process.env.API_KEY_SERVER;
+const apiKey = process.env.API_KEY; // api key
+const audId = process.env.AUD_ID; // audience id
+const apiKeyServer = process.env.API_KEY_SERVER; // server, like us-1, us-2 etc
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -20,32 +21,35 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.get("/failure.html", (req, res) => {
+  res.redirect("/");
+});
+
 mailchimp.setConfig({
   apiKey: apiKey,
   server: apiKeyServer,
 });
 
 app.post("/", function (req, res) {
-  const firstName = req.body.fName;
-  const secondName = req.body.lName;
-  const email = req.body.email;
-  const listId = audId;
-  const subscribingUser = {
-    firstName: firstName,
-    lastName: secondName,
-    email: email,
-  };
   const run = async () => {
     const response = await mailchimp.lists.batchListMembers(audId, {
-      members: [{
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: subscribingUser.firstName,
-          LNANE: subscribingUser.lastName,
-        }
-      }]
+      members: [
+        {
+          email_address: req.body.email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: req.body.fName,
+            LNAME: req.body.lName
+          },
+        },
+      ],
     });
+    if (response.errors[0] === undefined){
+      res.sendFile(__dirname + "/sucess.html");
+    }else{
+      res.sendFile(__dirname + "/failure.html");
+    }
   };
+
   run();
 });
