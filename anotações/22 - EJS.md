09e5723ecf0644d5f0339f13e6529ff3d2695998
JefteMartins <jefte.martins@hotmail.com>
2022-04-03 19:51:52 -0300

    adicionando uma lista com input

--- anotações/22 - EJS.md
+++ anotações/22 - EJS.md
@@ -1,5 +1,7 @@
 # EJS
 
+### ***Embedded JavaScript***
+
 ## 263. What We'll Make: A ToDoList
 
 falando que vamos aprender sobre partials e que faremos uma todo list
@@ -40,3 +42,35 @@ var today = new Date();
 falando sobre criar um código padrao pra ir substituindo em paginas que vao se repetir muito
 
 ## 266. Creating Your First EJS Templates
+
+fazendo template com ejs
+ejs.co
+
+`npm i ejs` instalar no projeto
+` app.use("view engine", "ejs");`
+
+o app.use tem que ficar sempre abaixo da constante que chama o express
+
+ele carrega arquivos dentro das `views`que estão dentro de uma pasta com esse mesmo nome que vc cria
+
+esses arquivos sao html e vc passa parametros pra esse arquivo html usando
+`<%= variavel %>` coo uma tag html
+
+`<h1>It's a <%= kindOfDay %>!</h1>` 
+
+para passar o parametro no .js vc deve usar 
+
+```javascript
+day = "Weekend";
+res.render("list", {kindOfDay: day});
+```
+
+sendo render a nova função, passando o nome do arquivo dentro do views e um objeto javascript passando a informação que será transferida.
+
+## 267. Running Code Inside the EJS Template
+
+usando a tag <% codigo %> em cada linha de JS que escrever
+
+## 268. Passing Data from Your Webpage to Your Server
+
+Com form normal usando body parse
--- práticasRápidas/Node-Express/Pratica-7-ToDo-List-v1/app.js
+++ práticasRápidas/Node-Express/Pratica-7-ToDo-List-v1/app.js
@@ -1,47 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
+let items = ["um", "dois", "tres"];
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = parseDay(currentDay);
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  res.render("list", {kindOfDay: day});
-
+  let day = today.toLocaleDateString("pt-BR", options);
+  res.render("list", { kindOfDay: day, newListItem: items });
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
 
-function parseDay(currentDay) {
-  let day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-  }
-  return day;
-}
+app.post("/", (req, res) => {
+  let item = req.body.todo;
+  items.push(item);
+  res.redirect("/");
+});
